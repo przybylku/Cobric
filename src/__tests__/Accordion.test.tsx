5dc79fd973febcbbaa7961cedb90304bda5e840f
Hubert Przybylski <49905947+przybylku@users.noreply.github.com>
2024-10-23 01:58:53 +0200

    (feat) Accordion main structure

--- src/__tests__/Accordion.test.tsx
+++ src/__tests__/Accordion.test.tsx
@@ -3,9 +3,23 @@
  */
 
 import { render, screen } from '@testing-library/react'
-import Accordion from '../components/Accordion'
+import Accordion, {
+  AccordionBody,
+  AccordionHeader,
+  AccordionItem
+} from '../components/Accordion'
 import React from 'react'
 test('is rendered', () => {
-  render(<Accordion anim={true}>a</Accordion>)
-  expect(screen.getByText('a')).toBeDefined()
+  render(
+    <Accordion anim={true}>
+      <AccordionItem>
+        <AccordionHeader>test</AccordionHeader>
+        <AccordionBody>
+          <p>tego typu</p>
+        </AccordionBody>
+      </AccordionItem>
+    </Accordion>
+  )
+  expect(document.getElementsByClassName('acccordtion-body')).toBeDefined()
+  expect(screen.getByText('tego typu')).toBeDefined()
 })
--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -3,6 +3,17 @@ import { AccordionProps } from '../types/types'
 
 /**@typedef {import("./types/types").AccordionProps} AccordionProps */
 
+// declare global {
+//   namespace JSX {
+//     interface AccordionNode extends React.ReactElement<any, any> {
+//       name: string
+//     }
+//     interface p extends React.ReactElement<any, any> {
+//       name: string
+//     }
+//   }
+// }
+
 /**
  * Creates Main component for Accordion
  * @param {boolean} anim - Accordion animation?
@@ -10,4 +21,25 @@ import { AccordionProps } from '../types/types'
  */
 export default function Accordion({ anim, children }: AccordionProps) {
   return <div className='accordion'>{anim && children}</div>
+}
+
+/**
+ *
+ * @param {React.ReactNode} children Have to be a AccordrionHeader or AccordionBody
+ * @returns {React.ReactNode} AccordionItem
+ */
+export function AccordionItem({ children }: { children: React.ReactNode }) {
+  return <div className='accordion-item'>{children}</div>
+}
+
+export function AccordionHeader({ children }: { children: React.ReactNode }) {
+  return (
+    <div className='accordion-header'>
+      <p className='accordion-header-p'>{children}</p>
+    </div>
+  )
+}
+
+export function AccordionBody({ children }: { children: React.ReactNode }) {
+  return <div className='accordion-body'>{children}</div>
 }
